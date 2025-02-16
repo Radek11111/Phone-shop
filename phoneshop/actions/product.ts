d0bd7196aef5ef7da2d3031272ab01188d7ab0e6
Radosław Łach <radek.lach92@gmail.com>
2025-02-16 13:02:02 +0100

    Created Steps to Purchase Products

--- phoneshop/actions/product.ts
+++ phoneshop/actions/product.ts
@@ -1,21 +1,23 @@
 import axios from "axios";
 
 export const getProductById = async (id: string | number) => {
-    try {
-
-        const response = await axios.get(`https://dummyjson.com/products/${id}`);
-        return response.data;
-    } catch (error: any) {
-        return { error: error.message };
-    }
+  try {
+    const response = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + `/products/${id}`
+    );
+    return response.data;
+  } catch (error: any) {
+    return { error: error.message };
+  }
 };
 
 export const getProducts = async () => {
-    try {
-        const response = await axios.get('https://dummyjson.com/products/search?q=phone');
-        return response.data;
-        
-    } catch (error: any) {
-        return { error: error.message };
-    }
-}
+  try {
+    const response = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + "/products/search?q=phone"
+    );
+    return response.data;
+  } catch (error: any) {
+    return { error: error.message };
+  }
+};
