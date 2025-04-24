6d5b754528a8f9539fb38a94095318773002d2cc
Radosław Łach <radek.lach92@gmail.com>
2025-04-24 16:51:57 +0200

    .

--- phoneshop/src/app/(website)/(pages)/checkout/layout.tsx
+++ phoneshop/src/app/(website)/(pages)/checkout/layout.tsx
@@ -1,5 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import Steps from "@/components/temp";
+import Steps from "@/components/Steps";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
